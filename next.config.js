78bcf56eb3e0e8120161b1ff96bf310235c5dc34
yend724 <yend9524@gmail.com>
2023-10-21 12:24:15 +0900

    Add Static Exports Setting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     dirs: ['src'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
