c506e6c480cb3d61815f1c1b425d58e2cbef6e2e
yend724 <yend9524@gmail.com>
2023-10-25 22:51:52 +0900

    Update DeviceOrientaion

--- src/app/_components/DeviceOrientation/DeviceOrientation.tsx
+++ src/app/_components/DeviceOrientation/DeviceOrientation.tsx
@@ -4,14 +4,13 @@ import { useDeviceOrientation } from '@/app/_hooks/useDeviceOrientation';
 import { DeviceOrientationLineGraph } from './DeviceOrientationLineGraph';
 import { DeviceOrientationValues } from './DeviceOrientationValues';
 
+const length = 60 * 30 + 1;
 export const DeviceOrientation = () => {
   const { alpha, gamma, beta } = useDeviceOrientation();
 
   const orientationRef = useRef({ alpha, gamma, beta });
   orientationRef.current = { alpha, gamma, beta };
-
   const [size, setSize] = useState({ width: 0, height: 0 });
-
   const [orientationData, setOrientationData] = useState([
     {
       timestamp: Date.now(),
@@ -38,30 +37,29 @@ export const DeviceOrientation = () => {
     };
   }, []);
 
-  const updateData = useCallback(() => {
-    const now = Date.now();
-    const length = 60 * 30 + 1;
-    setOrientationData((prev) => {
-      const newData = [
-        {
-          timestamp: now,
-          alpha: orientationRef.current.alpha,
-          gamma: orientationRef.current.gamma,
-          beta: orientationRef.current.beta,
-        },
-        ...prev,
-      ];
-      return newData.slice(0, length);
-    });
-    requestAnimationFrame(updateData);
-  }, []);
-
   useEffect(() => {
-    const requestAnimationFrameId = requestAnimationFrame(updateData);
+    let requestAnimationFrameId: number;
+    const loop = () => {
+      const now = Date.now();
+      setOrientationData((prev) => {
+        const newData = [
+          {
+            timestamp: now,
+            alpha: orientationRef.current.alpha,
+            gamma: orientationRef.current.gamma,
+            beta: orientationRef.current.beta,
+          },
+          ...prev,
+        ];
+        return newData.slice(0, length);
+      });
+      requestAnimationFrameId = requestAnimationFrame(loop);
+    };
+    loop();
     return () => {
       cancelAnimationFrame(requestAnimationFrameId);
     };
-  }, [updateData]);
+  }, []);
 
   return (
     <div className="grid grid-cols-[1fr_auto] gap-x-8">
