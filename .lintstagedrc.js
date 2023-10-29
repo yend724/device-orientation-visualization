5481f4d8e005f8874704ba63a3af22a1eabb7ca9
yend724 <yend9524@gmail.com>
2023-10-29 14:39:47 +0900

    Update lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,17 @@
-const path = require('path')
+const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
 
 const buildPrettierCommand = (filenames) =>
-  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`
+  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')}`;
+
+const buildTestCommand = () => `jest`;
 
 module.exports = {
+  'src/**/*.test.{js,jsx,ts,tsx,json}': [buildTestCommand],
   'src/**/*.{js,jsx,ts,tsx,json}': [buildPrettierCommand],
   'src/**/*.{js,jsx,ts,tsx,json}': [buildEslintCommand],
-}
+};
